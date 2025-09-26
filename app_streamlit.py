e037d4a91064e0f79431c4c0e1a1b5ec449a20fe
alicececot <ALI.CECOT@stud.uniroma3.it>
2025-09-26 16:08:39 +0200

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -922,6 +922,7 @@ def screen_seed_select(data: List[Dict]):
                     if is_sel: sel.remove(gid)
                     else:      sel.add(gid)
                     st.session_state.seed_selected_ids = list(sel)
+                    st.rerun()
 
                 st.markdown("</div>", unsafe_allow_html=True)
 
